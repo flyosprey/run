943b49f1c57e7154d352bb9fe0d01a24c83980e8
flyosprey <70664311+flyosprey@users.noreply.github.com>
2020-09-06 14:44:45 +0300

    24th commit

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Question
 
-# Create your tests here.
+
+class QuestionModelTest(TestCase):
+    """
+    was_published_recently() returns False for questions whose pub_date
+    is in the future.
+    """
+    def test_was_published_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
