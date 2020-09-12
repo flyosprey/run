72c03eb64f3416f8d4cc5be9ddd6201ffaa9209a
flyosprey <70664311+flyosprey@users.noreply.github.com>
2020-09-12 10:07:33 +0300

    28th commit

--- polls/tests.py
+++ polls/tests.py
@@ -62,6 +62,28 @@ class QuestionIndexViewTest(TestCase):
         )
 
 
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        '''
+        The detail view of a question with a pub_date in the future
+        return a 404 not found
+        '''
+        future_question = create_question(question_text='Future question', days=5)
+        url = reverse('polls:datail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        '''
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        '''
+        past_question = create_question(question_text='Past question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionModelTest(TestCase):
     def test_was_published_with_future_question(self):
         """
